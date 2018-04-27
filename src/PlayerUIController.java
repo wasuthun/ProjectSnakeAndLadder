177dc7398eae96c1fc5bf49aa796af3fa293812d
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2018-04-27 13:48:34 +0700

    update thread

--- src/PlayerUIController.java
+++ src/PlayerUIController.java
@@ -4,6 +4,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PlayerUIController {
 	@FXML
@@ -15,19 +18,30 @@ public class PlayerUIController {
 	@FXML
 	private ProgressBar time;
 	@FXML
-	private TextArea textArea1;
+	private TextField textField1;
 	@FXML
-	private TextArea textArea2;
-	private Game game;
+	private TextField textField2;
 	@FXML
+	private ImageView imageV1;
+	@FXML
+	private Image image; 
+	
+	private Player player1=new Player();
+	@FXML
+	
 	public void initialize() {
-		game=Game.getInstance();
 		roll.setOnAction(this::handleRoll);
 	}
 	@FXML
 	public void handleRoll(ActionEvent event) {
-//		textArea1.setText(""+player.getDice().getFirstDie());
-//		textArea2.setText(""+player.getDice().getSecondDie());
-		
+		player1.getDice().roll();
+		textField1.setText(""+player1.getDice().getFirstDie());
+		textField2.setText(""+player1.getDice().getSecondDie());
+//		if(player1.getDice().getFirstDie()==1) {
+//			image = new Image("/face1.jpg");
+//		} 
+//		
+//		imageV1=new ImageView();
+//		imageV1.setImage(image);
 	}
 }
